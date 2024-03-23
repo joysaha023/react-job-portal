474ae32a88b98265072017dd60dd54b09b383715
Joy <github.joysaha@gmail.com>
2024-03-23 11:47:31 +0600

    card item add

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <h2> banner page</h2>
+            <h2 className='text-center text-5xl'> banner page</h2>
         </div>
     );
 };
